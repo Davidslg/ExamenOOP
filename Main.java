23a8331f85b06070693d196eba5b2663b1431c86
Davidslg <davidslg.65@gmail.com>
2025-06-26 11:03:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,30 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Firma firma = new Firma("TechSoft");
 
+        Angajat a1 = new AngajatCuSalarFix("Maria", 3000);
+        Angajat a2 = new AngajatCuSalarFix("Ion", 2800);
+       
+        Angajat a3 = new AngajatCuOra("Andrei", 25, 160);
+        Angajat a4 = new AngajatCuOra("Ioana", 22, 170);
+
+        firma.angajeaza(a1);
+        firma.angajeaza(a2);
+        firma.angajeaza(a3);
+        firma.angajeaza(a4);
+
+        System.out.println("Angajatii firmei:");
+        firma.afiseazaAngajati();
+
+        System.out.println("Salariul mediu: " + firma.salariuMediu());
+
+        Angajat a5 = new AngajatCuSalarFix("Maria", 4000);
+        if (!firma.angajeaza(a5)) {
+            System.out.println("Nu se poate angaja aceeasi persoana de doua ori.");
+        }
+
+        ((AngajatCuSalarFix) a1).schimbaSalarFix(3500);
+        System.out.println("Salariul mediu dupa modificare: " + firma.salariuMediu());
+    }
 }
