8c9a0694599139f00f0500ebcacfeb2672742d3e
Davidslg <davidslg.65@gmail.com>
2025-06-26 10:58:05 +0300

    Update Angajat.java

--- Angajat.java
+++ Angajat.java
@@ -1,5 +1,21 @@
 package examen;
 
 public abstract class Angajat {
+    private String nume;
 
+    public Angajat(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public abstract double calculSalar();
+
+    @Override
+    public String toString() {
+        return "Angajat: " + nume;
+    }
 }
+
