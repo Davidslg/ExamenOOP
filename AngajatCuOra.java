a53ba800c602cfe890f0a125f4705a88728edc7c
Davidslg <davidslg.65@gmail.com>
2025-06-26 10:58:47 +0300

    Update AngajatCuOra.java

--- AngajatCuOra.java
+++ AngajatCuOra.java
@@ -1,5 +1,26 @@
 package examen;
 
-public class AngajatCuOra {
+public class AngajatCuOra extends Angajat {
+    private double tarifPeOra;
+    private int nrOre;
 
+    public AngajatCuOra(String nume, double tarifPeOra, int nrOre) {
+        super(nume);
+        this.tarifPeOra = tarifPeOra;
+        this.nrOre = nrOre;
+    }
+
+    @Override
+    public double calculSalar() {
+        return tarifPeOra * nrOre;
+    }
+
+    public void schimbaTarif(double nou) {
+        tarifPeOra = nou;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Tarif: " + tarifPeOra + " | Ore: " + nrOre;
+    }
 }
