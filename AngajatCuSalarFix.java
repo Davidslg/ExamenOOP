d44bc4da4a468b72a5b8f16d7c49a0f4a3d45999
Davidslg <davidslg.65@gmail.com>
2025-06-26 11:00:18 +0300

    Update AngajatCuSalarFix.java

--- AngajatCuSalarFix.java
+++ AngajatCuSalarFix.java
@@ -1,5 +1,25 @@
 package examen;
 
-public class AngajatCuSalarFix {
+public class AngajatCuSalarFix extends Angajat {
+    private double salarFix;
 
+    public AngajatCuSalarFix(String nume, double salarFix) {
+        super(nume);
+        this.salarFix = salarFix;
+    }
+
+    @Override
+    public double calculSalar() {
+        return salarFix;
+    }
+
+    public void schimbaSalarFix(double nou) {
+        salarFix = nou;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Salariu fix: " + salarFix;
+    }
 }
+
